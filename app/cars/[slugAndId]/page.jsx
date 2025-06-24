ca531c4e0a89d73e2d66154a73b813a0e51982d2
arpit <arpit@keuro.life>
2025-06-24 21:38:29 +0530

    update the env

--- app/cars/[slugAndId]/page.jsx
+++ app/cars/[slugAndId]/page.jsx
@@ -1,4 +1,5 @@
 import CarDetails from "./CarDetails";
+export const dynamic = 'force-dynamic';
 
 export default function CarDetailsPage({ params }) {
   return <CarDetails slugAndId={params.slugAndId} />;
